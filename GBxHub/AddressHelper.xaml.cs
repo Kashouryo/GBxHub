7cbb9c69323a367e1e5533f665f1f72ccc783553
Icesythe7 <lucasloveless90@gmail.com>
2020-07-12 14:47:17 -0400

    Cleaned and optimized addresshelper

--- GBxHub/AddressHelper.xaml.cs
+++ GBxHub/AddressHelper.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace GBxHub
 {
@@ -26,96 +17,57 @@ namespace GBxHub
 
         private void addressCheckBox_Click(object sender, RoutedEventArgs e)
         {
-            int adrNumber = 0;
-            string description = "No information available";
-            bool wrEnabled = wrCheckBox.IsChecked ?? false;
-            adrNumber += Convert.ToInt32(a0CheckBox.IsChecked ?? false) * 1;
-            adrNumber += Convert.ToInt32(a1CheckBox.IsChecked ?? false) * 2;
-            adrNumber += Convert.ToInt32(a2CheckBox.IsChecked ?? false) * 4;
-            adrNumber += Convert.ToInt32(a3CheckBox.IsChecked ?? false) * 8;
-            adrNumber += Convert.ToInt32(a4CheckBox.IsChecked ?? false) * 16;
-            adrNumber += Convert.ToInt32(a5CheckBox.IsChecked ?? false) * 32;
-            adrNumber += Convert.ToInt32(a6CheckBox.IsChecked ?? false) * 64;
-            adrNumber += Convert.ToInt32(a7CheckBox.IsChecked ?? false) * 128;
-            adrNumber += Convert.ToInt32(a8CheckBox.IsChecked ?? false) * 256;
-            adrNumber += Convert.ToInt32(a9CheckBox.IsChecked ?? false) * 512;
-            adrNumber += Convert.ToInt32(a10CheckBox.IsChecked ?? false) * 1024;
-            adrNumber += Convert.ToInt32(a11CheckBox.IsChecked ?? false) * 2048;
-            adrNumber += Convert.ToInt32(a12CheckBox.IsChecked ?? false) * 4096;
-            adrNumber += Convert.ToInt32(a13CheckBox.IsChecked ?? false) * 8192;
-            adrNumber += Convert.ToInt32(a14CheckBox.IsChecked ?? false) * 16384;
-            adrNumber += Convert.ToInt32(a15CheckBox.IsChecked ?? false) * 32768;
-            string addrStr = adrNumber.ToString("X");
+            var bNum = 0;
+            var description = "No information available";
+            var wrEnabled = wrCheckBox.IsChecked ?? false;
+            var adrNumber = checkBoxGrid.Children.OfType<CheckBox>().Sum(ctrl => Convert.ToInt32(ctrl.IsChecked ?? false) * (1 << bNum++));
+            var addrStr = adrNumber.ToString("X");
+
             addressBox.Text = addrStr;
             addressDecBox.Text = adrNumber.ToString();
 
             if (noMapperRadioButton.IsChecked ?? false)
             {
-                if (adrNumber <= 32767)
+                if (adrNumber <= 0x7FFF)
                 {
-                    int bankNum = (adrNumber <= 16383) ? 0 : 1;
-                    description = "Gameboy can read data from ROM address " + adrNumber.ToString("X") + ".";
+                    var bankNum = (adrNumber <= 0x3FFF) ? 0 : 1;
+                    description = $"Gameboy can read data from ROM address {adrNumber:X}.";
                     description += $"Which is in bank {bankNum}.";
                 }
                 else
-                {
                     description = "Flash ROM disabled.";
-                }
-
             }
             else if (mbc1RadioButton.IsChecked ?? false)
             {
-                if (adrNumber <= 32767)
+                if (adrNumber <= 0x7FFF)
                 {
                     if (wrEnabled)
                     {
                         //0000-1FFF - ROM Bank Number (Write Only)
-                        if (adrNumber >= 0 && adrNumber <= 8191)
-                        {
+                        if (Enumerable.Range(0x0, 0x1FFF).Contains(adrNumber))
                             description = "Gameboy can write to here to enable or disable RAM:\n00h - Disable RAM (default)\n0Ah = Enable RAM";
-                        }
                         //2000-3FFF - ROM Bank Number (Write Only)
-                        else if(adrNumber >= 8192 && adrNumber <= 16383)
-                        {
+                        else if (Enumerable.Range(0x2000, 0x3FFF).Contains(adrNumber))
                             description = "Gameboy can write additional address to MBC with data line";
-                        }
                         //4000-5FFF - RAM Bank Number - or - Upper Bits of ROM Bank Number (Write Only)
-                        else if (adrNumber >= 16384 && adrNumber <= 24575)
-                        {
+                        else if (Enumerable.Range(0x4000, 0x5FFF).Contains(adrNumber))
                             description = "This 2bit register can be used to select a RAM Bank in range from 00-03h, or to specify the upper two bits (Bit 5-6) of the ROM Bank number, depending on the current ROM/RAM Mode.";
-                        }
                         //6000-7FFF - ROM/RAM Mode Select (Write Only)
-                        else if (adrNumber >= 24576 && adrNumber <= 32767)
-                        {
+                        else if (Enumerable.Range(0x6000, 0x7FFF).Contains(adrNumber))
                             description = "Gameboy can write to here to change ROM/RAM mode:\n00h - ROM Banking Mode (up to 8KByte RAM, 2MByte ROM) (default)\n01h - RAM Banking Mode (up to 32KByte RAM, 512KByte ROM)";
-                        }
                     }
                     //0000-3FFF - ROM Bank 00 (Read Only)
-                    else if (adrNumber <= 16383)
-                    {
-                        description = "Gameboy can read data from ROM address " + adrNumber.ToString("X") + ". Which is in bank 0.";
-                    }
+                    else if (adrNumber <= 0x3FFF)
+                        description = $"Gameboy can read data from ROM address {adrNumber:X}. Which is in bank 0.";
                     //4000-7FFF - ROM Bank 01-7F (Read Only)
                     else
-                    {
                         description = "Gameboy can read data from ROM address which controlled by the MBC1";
-                    }
                 }
                 //A000-BFFF - RAM Bank 00-03, if any (Read/Write)
-                else if (adrNumber >= 40960 && adrNumber <= 49151)
-                {
-                    if (wrEnabled)
-                    {
-                        description = $"Gameboy can write data from RAM(if any) address {adrNumber.ToString("X")}.";
-                    }
-                    else
-                    {
-                        description = $"Gameboy can read data from RAM(if any) address {adrNumber.ToString("X")}.";
-                    }
-                }
-
+                else if (Enumerable.Range(0xA000, 0xBFFF).Contains(adrNumber))
+                    description = wrEnabled ? $"Gameboy can write data from RAM(if any) address {adrNumber:X}." : $"Gameboy can read data from RAM(if any) address {adrNumber:X}.";
             }
-        meaningTextBlock.Text = description;
+            meaningTextBlock.Text = description;
         }
     }
 }
